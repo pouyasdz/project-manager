6f88d6e17ec2ea040dacdebde1f5c6a4fee370b6
unknown <qpdevil83@gmail.com>
2022-07-14 20:46:58 +0430

    fix port number with env

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const Application = require("./app/server");
 const DB_ULR = "mongodb://localhost:27017/ProjectManager";
 require('dotenv').config();
-new Application(3000, DB_ULR)
+new Application(process.env.PORT || 3000, DB_ULR)
