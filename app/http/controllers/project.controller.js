19188c2f9f942d9d1aa87549a152e947feb126e8
unknown <qpdevil83@gmail.com>
2022-07-31 15:14:01 +0430

    project sections

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -1,51 +1,80 @@
 const { ProjectModel } = require("../../models/project");
 
-class ProjectController{
-    async creatProject(req, res, next){
-        try {
-            const owner = req.user._id;
-            const {title, text, image, tags} = req.body;
-            const result = await ProjectModel.create({title, text, owner, image, tags})
-            if(!result) throw {status:400, message:'افزودن پروژه انجام نشد'}
-            return res.status(201).json({
-                status:201,
-                success:true,
-                message : 'پروژه با موفقیت ایجاد شد'
-            })
-        } catch (error) {
-            next(error)
-        }
+class ProjectController {
+  async creatProject(req, res, next) {
+    try {
+      const owner = req.user._id;
+      const { title, text, image, tags } = req.body;
+      const result = await ProjectModel.create({
+        title,
+        text,
+        owner,
+        image,
+        tags,
+      });
+      if (!result) throw { status: 400, message: "افزودن پروژه انجام نشد" };
+      return res.status(201).json({
+        status: 201,
+        success: true,
+        message: "پروژه با موفقیت ایجاد شد",
+      });
+    } catch (error) {
+      next(error);
     }
-    async getAllProject(req, res, next){
-        try {
-            const owner = req.user._id;
-            const projects = await ProjectModel.find({owner});
-            return res.status(200).json({
-                status:200,
-                success:true,
-                projects
-            })
-        } catch (error) {
-            next(error)
-        }
+  }
+  async getAllProject(req, res, next) {
+    try {
+      const owner = req.user._id;
+      const projects = await ProjectModel.find({ owner });
+      return res.status(200).json({
+        status: 200,
+        success: true,
+        projects,
+      });
+    } catch (error) {
+      next(error);
     }
-    getProjectById(){
-
+  }
+  async findProject(projectID, owner){
+    const project = await ProjectModel.findOne({ owner, _id: projectID });
+    if (!project) throw { status: 404, message: "پروژه یافت نشد" };
+    return project
+  }
+  async getProjectById(req, res, next) {
+    try {
+      const owner = req.user._id;
+      const projectID = req.params.id;
+      const project = await this.findProject(projectID, owner);
+      return res.status(200).json({
+        status: 200,
+        success: true,
+        project,
+      });
+    } catch (error) {
+      next(error);
     }
-    getAllProjectOfTeam(){
-
+  }
+  async removeProject(req, res, next) {
+    try {
+      const owner = req.user._id;
+      const projectID = req.params.id;
+      await this.findProject(projectID, owner);
+      const deletProjectResult = await ProjectModel.deleteOne({_id:projectID})
+      if(deletProjectResult.deletedCount == 0) throw {status:400, message:" پروژه حذف نشد "}
+      return res.status(200).json({
+        status:200,
+        success:true,
+        message:" با موفقیت حذف شد "
+      })
+    } catch (error) {
+      next(error)   
     }
-    getProjectOfUser(){
-
-    }
-    updateProject(){
-
-    }
-    removeProject(){
-
-    }
+  }
+  async getAllProjectOfTeam() {}
+  async getProjectOfUser() {}
+  async updateProject() {}
 }
 
 module.exports = {
-    ProjectController : new ProjectController()
-}
+  ProjectController: new ProjectController(),
+};
--- app/router/project.js
+++ app/router/project.js
@@ -4,6 +4,7 @@ const { expressValidatorMapper } = require("../http/middlewares/checkErrors");
 const { createProjectValidator } = require("../http/validations/project");
 const { uploadFile } = require("../modules/express-fileUpload");
 const fileUpload = require("express-fileupload");
+const { mongoIDValidator } = require("../http/validations/public");
 const router = require("express").Router();
 
 router.post(
@@ -15,6 +16,11 @@ router.post(
   expressValidatorMapper,
   ProjectController.creatProject
 );
+
+router.post("/list", checkLogin,ProjectController.getAllProject)
+router.post("/:id", checkLogin ,mongoIDValidator(), expressValidatorMapper, ProjectController.findProject)
+router.post("/remove/:id", checkLogin, mongoIDValidator(), expressValidatorMapper, ProjectController.removeProject)
+router.post("/edit/:id", checkLogin, mongoIDValidator(), expressValidatorMapper, ProjectController.updateProject)
 
 module.exports = {
   projectRoutes: router,
