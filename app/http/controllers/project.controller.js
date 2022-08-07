0c19cb3fd7ef63763cc1dc481aaa2a7a91d69ce5
unknown <qpdevil83@gmail.com>
2022-08-07 20:05:41 +0430

    update project & add filters

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -2,9 +2,8 @@ const autoBind = require("auto-bind");
 const { ProjectModel } = require("../../models/project");
 
 class ProjectController {
-
-  constructor(){
-    autoBind(this)
+  constructor() {
+    autoBind(this);
   }
 
   async creatProject(req, res, next) {
@@ -41,17 +40,16 @@ class ProjectController {
       next(error);
     }
   }
-  async findProject(projectID, owner){
+  async findProject(projectID, owner) {
     try {
       const project = await ProjectModel.findOne({ owner, _id: projectID });
       if (!project) throw { status: 404, message: "پروژه یافت نشد" };
-      return project
-      
+      return project;
     } catch (error) {
       console.log("error");
     }
   }
-async getProjectById(req, res, next) {
+  async getProjectById(req, res, next) {
     try {
       const owner = req.user._id;
       const projectID = req.params.id;
@@ -70,20 +68,50 @@ async getProjectById(req, res, next) {
       const owner = req.user._id;
       const projectID = req.params.id;
       await this.findProject(projectID, owner);
-      const deletProjectResult = await ProjectModel.deleteOne({_id:projectID})
-      if(deletProjectResult.deletedCount == 0) throw {status:400, message:" پروژه حذف نشد "}
+      const deletProjectResult = await ProjectModel.deleteOne({
+        _id: projectID,
+      });
+      if (deletProjectResult.deletedCount == 0)
+        throw { status: 400, message: " پروژه حذف نشد " };
+      return res.status(200).json({
+        status: 200,
+        success: true,
+        message: " با موفقیت حذف شد ",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async updateProject(req, res, next) {
+    try {
+      const owner = req.user._id;
+      const projectID = req.params.id;
+      const project = await this.findProject(projectID, owner);
+      const data = {...req.body};
+      Object.entries(data).forEach(([key, value])=> {
+        if(!["title", "text", "tags"].includes(key)) delete data[key];
+        if(["", " ", 0, null, undefined, NaN].includes(value)) delete data[key];
+        if(key == "tags"&& (data["tags"].constructor == Array)){
+              data["tags"] = data["tags"].filter(val => {
+                if(!["", " ", 0, null, undefined, NaN].includes(val)) return val
+              })
+        }
+        if(data["tags"].length ===0 ) delete data["tags"]
+      })
+
+      const updateResult = await ProjectModel.updateOne({_id : projectID}, {$set : data})
+      if(updateResult.modifiedCount == 0) throw {status:400, message:"به روز رسانی انجام نشد"}
       return res.status(200).json({
         status:200,
         success:true,
-        message:" با موفقیت حذف شد "
+        message:"بروزرسانی انجام شد"
       })
     } catch (error) {
-      next(error)   
+      next(error)
     }
   }
   async getAllProjectOfTeam() {}
   async getProjectOfUser() {}
-  async updateProject() {}
 }
 
 module.exports = {
